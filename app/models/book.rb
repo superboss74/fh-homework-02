cef135a010e5f35e2080daeff5d37bc784a68c69
Hugo Mejia <superboss74@protonmail.com>
2019-04-10 16:53:06 +0000

    Adding Search feature assets

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,9 +1,18 @@
 class Book < ApplicationRecord
 
+    def index
+        @books = Book.all
+        @search = params["search"]
+        if @search.present?
+          @title = @search["title"]
+          @books = Book.where(title: @title)
+        end
+    end
+
  #   def self.search(search)
  #       if search
  #           find(:all, :conditions ==> ["title LIKE ? OR author LIKE ? OR genre LIKE ? OR classification LIKE ? OR booktype LIKE ? OR year LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
  #       else
  #           find(:all)
  #       end
-    end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
+  # whitelisting IPs from whole networks
+  config.web_console.whiny_requests = false
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
