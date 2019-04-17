2506d4d752e684405e6ca469b26cec1d65dd0d01
Hugo Mejia <superboss74@protonmail.com>
2019-04-17 00:32:08 +0000

    Fixing Search Function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,10 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-      @books = Book.all
+    @books = Book.search(params[:keyword])
   end
 
-  # GET /books/1
+   # GET /books/1
   # GET /books/1.json
   def show
   end
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :genre, :classification, :booktype, :year, :search)
+      params.require(:book).permit(:title, :author, :genre, :classification, :booktype, :year, :keyword)
     end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,18 +1,9 @@
 class Book < ApplicationRecord
-
-    def index
-        @books = Book.all
-        @search = params["search"]
-        if @search.present?
-          @title = @search["title"]
-          @books = Book.where(title: @title)
-        end
-    end
-
- #   def self.search(search)
- #       if search
- #           find(:all, :conditions ==> ["title LIKE ? OR author LIKE ? OR genre LIKE ? OR classification LIKE ? OR booktype LIKE ? OR year LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
- #       else
- #           find(:all)
- #       end
+  def self.search(keyword)
+    if keyword.present?
+      Book.where("title ILIKE ? OR author ILIKE ? OR genre ILIKE ? OR classification ILIKE ? OR booktype ILIKE ?", keyword, keyword, keyword, keyword, keyword)
+    else
+      Book.all
+    end 
+  end
 end
